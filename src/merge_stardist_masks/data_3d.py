5a5b19599c7eb796c539880b1a1a15d8608e1147
Niklas Netter <niknett@gmail.com>
2022-11-28 07:51:42 +0100

    data 3d mypy passed

--- src/merge_stardist_masks/data_3d.py
+++ src/merge_stardist_masks/data_3d.py
@@ -1,20 +1,32 @@
 """Stardist 3D data generator for new weights and probability maps."""
+from typing import List
+from typing import Tuple
+from typing import TypeVar
+
 import numpy as np  # pragma: no cover
-from scipy.ndimage import zoom  # pragma: no cover
-from stardist.geometry import star_dist3D  # pragma: no cover
-from stardist.models.model3d import StarDistData3D  # pragma: no cover
-from stardist.sample_patches import sample_patches  # pragma: no cover
-from stardist.utils import edt_prob
-from stardist.utils import mask_to_categorical
+import numpy.typing as npt
+from scipy.ndimage import zoom  # type: ignore [import] # pragma: no cover
+from stardist.geometry import star_dist3D  # type: ignore [import] # pragma: no cover
+from stardist.models.model3d import (  # type: ignore [import] # pragma: no cover
+    StarDistData3D,
+)
+from stardist.sample_patches import (  # type: ignore [import] # pragma: no cover
+    sample_patches,
+)
+from stardist.utils import edt_prob  # type: ignore [import] # pragma: no cover
+from stardist.utils import mask_to_categorical  # pragma: no cover
 
 from .touching_pixels import bordering_gaussian_weights  # pragma: no cover
 from .touching_pixels import touching_pixels_3d  # pragma: no cover
 
 
-class OptimizedStarDistData3D(StarDistData3D):  # pragma: no cover
+T = TypeVar("T", bound=np.generic)
+
+
+class OptimizedStarDistData3D(StarDistData3D):  # type: ignore [misc] # pragma: no cover
     """Overwrite __getitem__ function to use different prob and weights."""
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int) -> Tuple[List[npt.NDArray[T]], List[npt.NDArray[T]]]:
         """Return batch i as numpy array."""
         idx = self.batch(i)
         arrays = [
@@ -46,7 +58,7 @@ class OptimizedStarDistData3D(StarDistData3D):  # pragma: no cover
         else:
             xs = np.stack(xs, out=self.out_X[: len(ys)])
         if xs.ndim == 4:  # input image has no channel axis
-            xs = np.expand_dims(xs, -1)
+            xs = np.expand_dims(xs, -1)  # type: ignore [no-untyped-call]
 
         tmp_prob_ = [
             edt_prob(lbl, anisotropy=self.anisotropy)[self.ss_grid[1:]] for lbl in ys
@@ -76,11 +88,13 @@ class OptimizedStarDistData3D(StarDistData3D):  # pragma: no cover
         else:
             dist = np.stack(tmp_dists, out=self.out_star_dist3D[: len(ys)])
 
-        prob = np.expand_dims(prob, -1)
-        dist_mask = np.expand_dims(dist_mask, -1)
+        prob = np.expand_dims(prob, -1)  # type: ignore [no-untyped-call]
+        dist_mask = np.expand_dims(dist_mask, -1)  # type: ignore [no-untyped-call]
 
         # append dist_mask to dist as additional channel
-        dist = np.concatenate([dist, dist_mask], axis=-1)
+        dist = np.concatenate(  # type: ignore [no-untyped-call]
+            [dist, dist_mask], axis=-1
+        )
 
         if self.n_classes is None:
             return [xs], [prob, dist]
