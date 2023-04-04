c56605cc41f2a71a55bb028cad055de897c45181
Niklas Netter <niknett@gmail.com>
2023-04-04 16:48:25 +0200

    tracking utilities added and calc_midpoints fixed

--- src/merge_stardist_masks/tracking.py
+++ src/merge_stardist_masks/tracking.py
@@ -1,5 +1,6 @@
 """Pre- and postprocessing for tracking by displacement maps."""
 from typing import Dict
+from typing import List
 from typing import Tuple
 
 import numpy as np
@@ -18,12 +19,13 @@ def calc_midpoints(lbl: npt.NDArray[np.int_]) -> Dict[int, npt.NDArray[np.double
     for i, obj in enumerate(objects):
         if obj is None:
             continue
-        tmp = lbl[obj]
+        lbl_id = i + 1
+        tmp = lbl[obj] == lbl_id
 
         indices = np.argwhere(tmp)
         offset = np.array([obj[i].start for i in range(lbl.ndim)])
 
-        midpoints[i + 1] = np.mean(indices, axis=0) + offset
+        midpoints[lbl_id] = np.mean(indices, axis=0) + offset
 
     return midpoints
 
@@ -121,6 +123,91 @@ def track_from_displacement_map_single_timepoint(
             i += 1
 
     return new_t1
+
+
+def only_track_from_displacement_map_single_timestep(
+    lbl_t0: npt.NDArray[np.int_],
+    lbl_t1: npt.NDArray[np.int_],
+    displacement_map: npt.NDArray[np.double],
+    points: npt.NDArray[np.int_],
+    threshold: float = 0.5,
+) -> Dict[int, int]:
+    """Find nearest object in previous timepoint for each object."""
+    tracked_ids = get_tracked_ids(lbl_t1, displacement_map, points, threshold=threshold)
+
+    midpoints_t0 = calc_midpoints(lbl_t0)
+
+    midpoints_t0_, lbl_ids0 = dict_to_array_indices(midpoints_t0)
+    orig_midpoints_t1_, lbl_ids1 = dict_to_array_indices(tracked_ids)
+    dist_matrix = distance_matrix(midpoints_t0_, orig_midpoints_t1_)
+
+    inds_t1 = np.argmin(dist_matrix, axis=0)
+
+    return {id_t1: id_t0 for id_t1, id_t0 in zip(lbl_ids1, lbl_ids0[inds_t1])}
+
+
+def only_track_from_displacement_map_multiple_timesteps(
+    lbls: npt.NDArray[np.int_],
+    displacement_maps: npt.NDArray[np.double],
+    threshold: float = 0.5,
+) -> List[Tuple[int, List[int]]]:
+    """Creates lists of label ids that resemble tracks through timeseries."""
+    bw_dicts = []
+    points = mesh_from_shape(lbls.shape[1:])
+    for i in range(1, lbls.shape[0]):
+        bw_dicts.append(
+            only_track_from_displacement_map_single_timestep(
+                lbls[i - 1],
+                lbls[i],
+                displacement_maps[i - 1],
+                points,
+                threshold=threshold,
+            )
+        )
+
+    tracks = [(0, [val, key]) for key, val in bw_dicts[0].items()]
+    for i, bw_dict in enumerate(bw_dicts):
+        if i == 0:
+            continue
+        for key, val in bw_dict.items():
+            found = False
+            for track_start_time, track in tracks:
+                current_track_time = track_start_time + len(track) - 1
+                if track[-1] == val and current_track_time == i:
+                    track.append(key)
+                    found = True
+            if not found:
+                tracks.append((i, [val, key]))
+    return tracks
+
+
+def track_array_from_tracking_list(
+    lbls: npt.NDArray[np.int_],
+    tracks: List[Tuple[int, List[int]]],
+) -> npt.NDArray[np.double]:
+    """Create an array describing tracks in the napari format."""
+    midpoints = [calc_midpoints(lbls[i]) for i in range(lbls.shape[0])]
+
+    track_list = []
+    for i in range(len(tracks)):
+        start_time, track = tracks[i]
+        for j, lbl_id in enumerate(track):
+            time = start_time + j
+            midpoint = midpoints[time][lbl_id]
+
+            track_list.append((i, time) + tuple(midpoint))
+    return np.array(track_list)
+
+
+def bw_dict_to_fw_dict(bw_dict: Dict[int, int]) -> Dict[int, List[int]]:
+    """Invert a backward pointing tracking dict into a forward pointing one."""
+    fw_dict = {}
+    for key, val in bw_dict:
+        if val not in fw_dict:
+            fw_dict[val] = [key]
+        else:
+            fw_dict[val].append(key)
+    return fw_dict
 
 
 def dict_to_array_indices(
