8c8db30dfd5d684a8c8962c20ea2168fdaee56e0
Niklas Netter <niknett@gmail.com>
2023-06-07 16:27:53 +0200

    no py3.7 tests anymore

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,9 @@ except ImportError:
 
 
 package = "merge_stardist_masks"
-python_versions = ["3.9", "3.8", "3.7"]
+# not using version 3.7 anymore as it has a problem with the new tf>=2.11.1 constraint
+# python_versions = ["3.9", "3.8", "3.7"]
+python_versions = ["3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
